fcbcb58fb8f6153833c028f834859f7ce79549c4
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2020-02-20 14:58:25 +0000

    Added script to save settings
    Addeda script to save the selected data from the main menu in the level scene

---
+++ Test #1/Boids Alpha/Assets/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    float brightness;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Loads the value the player selected for the brightness in the options menu
+        //And applies it to the current scene
+        brightness = PlayerPrefs.GetFloat("UserBrightness", 0);
+        RenderSettings.ambientLight = new Color(brightness, brightness, brightness, 1);//asigns the value selected from the options slider to the current settings brightness options
+        PlayerPrefs.SetFloat("UserBrightness", brightness);
+    }
+}
