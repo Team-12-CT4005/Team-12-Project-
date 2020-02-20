59b5a2ce0c7e9a630a5d38474068051c1dad74fc
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2020-02-20 15:50:15 +0000

    PauseMenu
    Added a functional pause menu

---
+++ Test #1/Boids Alpha/Assets/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject menu;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+    void Pause()
+    {
+        menu.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
